c14e3dd2e032724d5ee006ec492a0af1c7562640
Parallaax <acostatorres.by@gmail.com>
2022-09-05 22:50:17 -0300

    proyecto - iteracion fase 1 completa

---
+++ TurismoReal/catalogo/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.forms import fields
+from .models import Depto
+
+
+class DeptoForm(forms.ModelForm):
+
+    class Meta:
+        model = Depto
+        fields = '__all__'
