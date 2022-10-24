38a85877b06f10dd596cefbdd56a92062eb0362b
Parallaax <acostatorres.by@gmail.com>
2022-10-24 19:25:31 -0300

    Formateo de rut y listo para Merge

--- TurismoReal/catalogo/static/js/show.js
+++ TurismoReal/catalogo/static/js/show.js
@@ -9,3 +9,7 @@ eye.addEventListener("click", function(){
         eye.style.opacity = 0.2;
     }
 })
+
+function formateoRut(rut)
+    {rut.value=rut.value.replace(/[.-]/g, '')
+    .replace( /^(\d{1,2})(\d{3})(\d{3})(\w{1})$/, '$1.$2.$3-$4')}
